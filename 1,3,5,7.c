23b30bbfa86d9bbd5d3753854e8b32ed309ded19
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2021-11-29 20:03:13 +0530

    Create 1,3,5,7.c

---
+++ 1,3,5,7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=-1,i,n;
+	printf("Enter your last term ");
+	scanf("%d",&n);
+	for(i=1;i<n;i++)
+	{
+		a+=2;
+		printf("%d,",a);
+	}
+	printf("%d",a+=2);
+}
