858af6feaac4e8cecf350aaed4f9ef83f28044c4
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2021-11-29 20:50:42 +0530

    Update 1,3,5,7.c

--- 1,3,5,7.c
+++ 1,3,5,7.c
@@ -4,10 +4,13 @@ int main()
 	int a=-1,i,n;
 	printf("Enter your last term ");
 	scanf("%d",&n);
-	for(i=1;i<n;i++)
+	for(i=1;i<=n;i++)
 	{
 		a+=2;
-		printf("%d,",a);
+		printf("%d",a);
+		if(i<n)
+		{
+			printf(",");
+		}
 	}
-	printf("%d",a+=2);
 }
