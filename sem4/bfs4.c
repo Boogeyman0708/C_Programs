579de2e5ffcbf5a43ad17b7a5833398884b86485
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2024-01-20 20:52:16 +0530

    Create bfs4.c

---
+++ sem4/bfs4.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+#define MAX_SIZE 100
+
+int graph[MAX_SIZE][MAX_SIZE];
+int queue[MAX_SIZE];
+int front=0,rear=-1;
+bool visited[MAX_SIZE];
+
+void bfs(int start,int n)
+{
+	visited[start]=true;
+	queue[++rear]=start;
+	while(front<=rear)
+	{
+		int current= queue[front++];
+		printf("%d->",current);
+		for(int i=0;i<n;i++)
+		{
+			if(graph[current][i] && !visited[i])
+			{
+				visited[i]=true;
+				queue[++rear]=i;
+			}
+		}
+	}
+}
+int main()
+{
+	int n,i,j,start;
+	printf("Enter the number of vertices: ");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			scanf("%d",&graph[i][j]);
+		}
+	}
+	printf("\nEnter the starting vertex: ");
+	scanf("%d",&start);
+	printf("\nThe BFS Traversal: ");
+	bfs(start,n);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
