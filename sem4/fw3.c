05ddc1aab884086ddccb1dcba5e6c5b8ec861653
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2024-01-20 20:53:00 +0530

    Create fw3.c

---
+++ sem4/fw3.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+#define INFINITY 9999
+#define MAX_SIZE 100
+
+void FloydWarshall(int graph[MAX_SIZE][MAX_SIZE], int n)
+{
+	int dist[MAX_SIZE][MAX_SIZE];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			dist[i][j]= graph[i][j];
+		}
+	}
+	
+	for(int k=0;k<n;k++)
+	{
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(dist[i][k] + dist[k][j] < dist[i][j])
+				{
+					dist[i][j] = dist[i][k] + dist[k][j];
+				}
+			}
+		}
+	}
+	
+	printf("Shortest path between each pair of vertices: \n");
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(dist[i][j] == INFINITY)
+			{
+				printf("INF\t");
+			}
+			else
+			{
+				printf("%d\t",dist[i][j]);
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int graph[MAX_SIZE][MAX_SIZE], n;
+	printf("Enter the number of vertices: ");
+	scanf("%d",&n);
+	printf("\nEnter the adjacency martrix: \n");
+	for (int i=0;i<n;i++)
+	{
+		for (int j=0;j<n;j++)
+		{
+			scanf("%d",&graph[i][j]);
+		}
+	}
+	FloydWarshall(graph,n);
+	return 0;
+}
