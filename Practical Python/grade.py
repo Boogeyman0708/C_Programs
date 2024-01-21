15eaab79e0a9946f68990828ec9b401ec69ec9b3
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2024-01-21 21:30:33 +0530

    Create grade.py

---
+++ Practical Python/grade.py
@@ -0,0 +1,17 @@
+a=float(input("Enter the number: "))
+if(a>9.0 and a<10.0):
+    print("The grade of the student is: A++")
+elif(a>8.0 and a<9.0):
+    print("The grade of the student is: A+")
+elif(a>7.0 and a<8.0):
+    print("The grade of the student is: A")
+elif(a>6.0 and a<7.0):
+    print("The grade of the student is: B+")
+elif(a>5.0 and a<6.0):
+    print("The grade of the student is: B-")
+elif(a>4.0 and a<5.0):
+    print("The grade of the student is: QH")
+elif(a>3.0 and a<4.0):
+    print("The grade of the student is: P")
+elif(a<3.0):
+    print("The grade of the student is: F")
