21de8de547e774ede0bbdf12dc40d7248c630d38
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2024-02-12 00:00:32 +0530

    Create arrayavg.java

---
+++ java/src/arrayavg.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class arrayavg {
+    public static void main(String[] args) {
+        Scanner SC = new Scanner(System.in);
+        System.out.print("enter n terms");
+        int n = SC.nextInt();
+        int a[] = new int[n];
+        System.out.println("enter the values one by one");
+        for (int i = 0; i < n; i++) {
+            a[i] = SC.nextInt();
+        }
+        int s = 0;
+        for (int i = 0; i < n; i++) {
+            s = s + a[i];
+        }
+        SC.close();
+        double avg = 0.0;
+        avg = s / n;
+        System.out.println("The average of the array is: " + avg);
+    }
+}
