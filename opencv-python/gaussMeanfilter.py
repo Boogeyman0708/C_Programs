4df84a3ee34d77defc1e8f0e1febd551979d884d
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2024-02-12 00:02:34 +0530

    Create gaussMeanfilter.py

---
+++ opencv-python/gaussMeanfilter.py
@@ -0,0 +1,23 @@
+'''Gaussian noise'''
+
+import cv2
+import numpy as np
+import random
+im=cv2.imread('img/sample1.jpg',1)
+img=cv2.resize(im,(520,440))
+
+img1= img.copy()
+img2= img.copy()
+
+cv2.randn(img2,(0,0,0),(50,50,50))
+
+img3= cv2.add(img2,img1)
+
+cv2.imshow('img1',img1)
+cv2.imshow('img3',img3)
+
+
+arithmatic_mean= cv2.blur(img3,(3,3))
+cv2.imshow('mean',arithmatic_mean)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
