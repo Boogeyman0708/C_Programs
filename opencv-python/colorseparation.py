ba81eccce538d00548739570a31261f41e38fbef
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2024-02-12 00:02:52 +0530

    Create colorseparation.py

---
+++ opencv-python/colorseparation.py
@@ -0,0 +1,21 @@
+'''Write a program to perform color separation into R, G, and B from a color image. '''
+
+import cv2
+
+img= cv2.imread('img/sample1.jpg')
+
+blue, green, red  = cv2.split(img)
+
+cv2.imshow('Original',img)
+cv2.waitKey(0)
+
+cv2.imshow('Without Blue',blue)
+cv2.waitKey(0)
+
+cv2.imshow('Without Green',green)
+cv2.waitKey(0)
+
+cv2.imshow('Without Red',red)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
