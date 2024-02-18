bd03d2ed579dd752a5bf93446489229b85355bf9
Boogeyman0708 <94966806+Boogeyman0708@users.noreply.github.com>
2024-02-18 23:59:24 +0530

    Create circle.py

---
+++ Practical Python/circle.py
@@ -0,0 +1,16 @@
+import math
+
+def area(x):
+    area = math.pi*r*r
+    return(area)
+
+def perimeter(x):
+    peri = 2*math.pi*x
+    return (peri)
+
+r=float(input("Enter the radius of the circle: "))
+
+circle_area = area(r)
+circle_peri = perimeter(r)
+
+print("The Area of the circle:  {0}\nThe Perimeter of the circle:   {1}".format(circle_area,circle_peri))
